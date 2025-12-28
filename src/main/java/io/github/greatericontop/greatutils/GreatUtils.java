1af2006976fd6141e7afe4abc15b59549cd29046
greatericontop <greateric@protonmail.com>
2025-12-27 23:42:10 -0500

    add canceltask command

--- src/main/java/io/github/greatericontop/greatutils/GreatUtils.java
+++ src/main/java/io/github/greatericontop/greatutils/GreatUtils.java
@@ -1,15 +1,10 @@
 package io.github.greatericontop.greatutils;
 
-import io.github.greatericontop.greatutils.commandexecution.CommandTask;
+import io.github.greatericontop.greatutils.commandexecution.CancelTaskCommand;
 import io.github.greatericontop.greatutils.commandexecution.CommandTaskManager;
 import io.github.greatericontop.greatutils.commandexecution.ExecLaterCommand;
 import io.github.greatericontop.greatutils.commandexecution.MyTasksCommand;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 
 public class GreatUtils extends JavaPlugin {
 
@@ -21,6 +16,7 @@ public class GreatUtils extends JavaPlugin {
 
         this.getCommand("execlater").setExecutor(new ExecLaterCommand(this));
         this.getCommand("mytasks").setExecutor(new MyTasksCommand(this));
+        this.getCommand("canceltask").setExecutor(new CancelTaskCommand(this));
     }
 
 }
---
+++ src/main/java/io/github/greatericontop/greatutils/commandexecution/CancelTaskCommand.java
@@ -0,0 +1,48 @@
+package io.github.greatericontop.greatutils.commandexecution;
+
+import io.github.greatericontop.greatutils.GreatUtils;
+import io.github.greatericontop.greatutils.util.GreatCommands;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+
+public class CancelTaskCommand implements CommandExecutor {
+
+    private final GreatUtils plugin;
+    public CancelTaskCommand(GreatUtils plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (args.length < 1) {
+            sender.sendMessage("§cSpecify what to cancel!");
+            return false;
+        }
+        if (args[0].equalsIgnoreCase("all")) {
+            List<CommandTask> tasks = plugin.commandTaskManager.get(sender);
+            if (tasks == null || tasks.isEmpty()) {
+                sender.sendMessage("§cYou have nothing to cancel!");
+                return true;
+            }
+            for (CommandTask task : tasks) {
+                Bukkit.getScheduler().cancelTask(task.taskId());
+            }
+            sender.sendMessage("§3Cancelled all your tasks!");
+            return true;
+        }
+        int[] taskIds = GreatCommands.argumentIntegerConsumeRest(0, args);
+        if (taskIds == null) {
+            sender.sendMessage("§cTask IDs must be integers!");
+            return false;
+        }
+        for (int taskId : taskIds) {
+            Bukkit.getScheduler().cancelTask(taskId);
+            sender.sendMessage("§3Cancelled task §f%d§3.".formatted(taskId));
+        }
+        return true;
+    }
+}
